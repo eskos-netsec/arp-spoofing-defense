71c6a2bd1a63df74069cfa426e6f21183dbded44
eskos-netsec <eskos.21_in@proton.me>
2025-04-12 10:44:11 -0500

    Create rsa_key.py

---
+++ rsa_key.py
@@ -0,0 +1,18 @@
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.asymmetric import rsa
+
+private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+public_key = private_key.public_key()
+
+with open("private.pem", "wb") as f:
+    f.write(private_key.private_bytes(
+        serialization.Encoding.PEM,
+        serialization.PrivateFormat.PKCS8,
+        serialization.NoEncryption()
+    ))
+
+with open("public.pem", "wb") as f:
+    f.write(public_key.public_bytes(
+        serialization.Encoding.PEM,
+        serialization.PublicFormat.SubjectPublicKeyInfo
+    ))
